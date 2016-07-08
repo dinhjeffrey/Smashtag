fa98aaa3f87bac0ffc307bdbc74556ccda5903de
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-08 10:05:21 -0700

    up to importing Twitter framework and display #stanford on tableView working

--- Smashtag/TweetTableViewController.swift
+++ Smashtag/TweetTableViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Twitter
+
 // first thing to do when creating new VC is what is my model?
 class TweetTableViewController: UITableViewController {
-
-    var tweets = [Array<Tweet>]() {
+    
+    var tweets = [Array<Twitter.Tweet>]() {
         didSet {
             tableView.reloadData()
         }
@@ -22,8 +24,30 @@ class TweetTableViewController: UITableViewController {
             title = searchText
         }
     }
+    private var twitterRequest: Twitter.Request? {
+        if let query = searchText where !query.isEmpty {
+            return Twitter.Request(search: query + " -filter:retweets", count: 100)
+        }
+        return nil
+    }
+    private var lastTwitterRequest: Twitter.Request?
+    // multithread. tableView that is scrolling up and down, it reuses cells. It might come back to a cell that is not display what it used to before
+    // two things about asynchronous:
+    // 1. break memory cycles
+    // 2. things come back may not come back as expected order
     private func searchForTweets() {
-        
+        if let request = twitterRequest {
+            lastTwitterRequest = request
+            request.fetchTweets { [weak weakSelf = self] newTweets in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if request == weakSelf?.lastTwitterRequest{
+                        if !newTweets.isEmpty {
+                            weakSelf?.tweets.insert(newTweets, atIndex: 0)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     
@@ -31,37 +55,41 @@ class TweetTableViewController: UITableViewController {
         super.viewDidLoad()
         searchText = "#stanford"
     }
-
-
-
+    
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return tweets.count
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tweets[section].count
     }
-
+    
+    private struct Storyboard {
+        static let TweetCellIdentifier = "Tweet"
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.TweetCellIdentifier, forIndexPath: indexPath)
+        
+        let tweet = tweets[indexPath.section][indexPath.row]
+        cell.textLabel?.text = tweet.text
+        cell.detailTextLabel?.text = tweet.user.name
+        
         return cell
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
