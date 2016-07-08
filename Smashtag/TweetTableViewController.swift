51809a2412e625589071620e3899ec65c36b1fa3
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-08 09:00:41 -0700

    updated to making a TableViewController and part of the model

---
+++ Smashtag/TweetTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by jeffrey dinh on 7/8/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+// first thing to do when creating new VC is what is my model?
+class TweetTableViewController: UITableViewController {
+
+    var tweets = [Array<Tweet>]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    private func searchForTweets() {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
+
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
