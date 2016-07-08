5422e7cf850148eef1b2332572988106937190e0
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-08 10:37:44 -0700

    updated to custom tableViewCell working

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by jeffrey dinh on 7/8/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    
+    var tweet: Twitter.Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    private func updateUI() {
+        // reset any existing tweet info
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel?.text = nil
+        
+        // load new info from our tweet (if any)
+        if let tweet = self.tweet {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            let formatter = NSDateFormatter()
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            } else {
+                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            }
+            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
